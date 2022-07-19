884399b51d9c90e3c89748debeec3256fd5bda4c
rohitsigar <rohitsigar05@gmail.com>
2022-07-19 12:17:56 +0530

    undefined vs not defined _06

---
+++ undefined vs not defined _06.js
@@ -0,0 +1,46 @@
+
+// var a;
+
+// console.log(a);
+
+
+// console.log(x);
+
+// output:
+// undefined
+
+// x is not defined 
+//--------------------------------------
+
+// var a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// a = "Hello World";
+
+// console.log(a);
+// a = true;
+// console.log(a);
+
+
+// output - 
+
+// undefined
+// 10
+// Hello World
+// true
+
+// javascript is a loosly typed language
+// also known as weekly typed language
+
+//-------------------------------------------------
+
+var a = 10;
+console.log(a);
+
+a = undefined;
+
+console.log(a);
+
+
+
