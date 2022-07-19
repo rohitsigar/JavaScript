44b7c36c788b5070242a5a808cf8c21da5ee901d
rohitsigar <rohitsigar05@gmail.com>
2022-07-19 16:40:37 +0530

    let & const in JS and Temporal dead Zone_08

---
+++ let & const in JS and Temporal dead Zone_08.js
@@ -0,0 +1,74 @@
+// console.log(a);
+
+// let a = 10;
+// var b = 100;
+
+
+ //output 
+// ReferenceError: Cannot access 'a' before initialization
+
+//-----------------------------------------------
+
+
+// let a = 10;
+// console.log(a);
+// var b = 100;
+
+
+ //output 
+// 10
+
+//------------------------------------------------
+
+// console.log(window.a);
+
+// let a = 10;
+
+// console.log(window.a);
+// console.log(this.a);
+ 
+
+ //output 
+// undefined
+// undefined
+
+//------------------------------------------------
+
+
+// let a = 100;
+
+// let a = 1000;
+
+// output
+// SyntaxError: Identifier 'a' has already been declared
+
+//------------------------------
+
+// var a  = 100;
+
+// var a  = 10000;
+
+
+//this is possible
+
+//-------------------------------------------
+
+// let a;
+// a = 10;
+
+
+// const b;
+
+// b = 100;
+
+
+
+// can do this with let but now with const
+
+//----------------------------------------------
+
+
+const a = 10;
+
+a = 200;
+
